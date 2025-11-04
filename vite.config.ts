ff31672ea21f89e104f23397ebcc681640d277dd
George Tolakis <tolakis@gmail.com>
2025-11-04 15:32:28 +0200

    vite config cix

--- vite.config.ts
+++ vite.config.ts
@@ -1,9 +1,20 @@
+// vite.config.ts
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
-import basicSsl from '@vitejs/plugin-basic-ssl'
 
-export default defineConfig({
-  plugins: [react(), basicSsl()],
-  server: { https: true, host: '127.0.0.1', port: 8443 },
-  preview: { https: true, host: '127.0.0.1', port: 4173 }
+export default defineConfig(async ({ command }) => {
+  const plugins = [react()]
+
+  if (command === 'serve') {
+    // only load SSL plugin in local dev
+    const basicSsl = (await import('@vitejs/plugin-basic-ssl')).default
+    plugins.push(basicSsl())
+  }
+
+  return {
+    plugins,
+    server: command === 'serve'
+      ? { https: true, host: '127.0.0.1', port: 8443 }
+      : undefined,
+  }
 })
